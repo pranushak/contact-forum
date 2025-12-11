b55ec9118d80971e019e3cf923999d9173b74dec
Pranusha Keerthipati <pranushak87@gmail.com>
2025-12-11 11:57:03 -0600

    added springboot backend

---
+++ instacart-backend/src/main/java/instacart_backend/instacart_backend/InstacartBackendApplication.java
@@ -0,0 +1,13 @@
+package instacart_backend.instacart_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstacartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstacartBackendApplication.class, args);
+	}
+
+}
---
+++ instacart-backend/src/test/java/instacart_backend/instacart_backend/InstacartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package instacart_backend.instacart_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstacartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
