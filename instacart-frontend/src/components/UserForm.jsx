3c0d068961dd5acd386f5648c8b55659c7ea315a
Pranusha Keerthipati <pranushak87@gmail.com>
2025-12-11 14:31:12 -0600

    Added userForm component

--- instacart-frontend/src/components/UserForm.jsx
+++ instacart-frontend/src/components/UserForm.jsx
@@ -1,5 +1,69 @@
 import React, { useState } from "react";
 
 const userForm = () => {
-    
-}
+     const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    age: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:8080/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (response.ok) {
+      alert("User saved successfully!");
+    } else {
+      alert("Error saving user");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} style={{ width: "300px" }}>
+      <h3>Create User</h3>
+
+      <input
+        type="text"
+        name="name"
+        placeholder="Enter Name"
+        value={formData.name}
+        onChange={handleChange}
+      /><br /><br />
+
+      <input
+        type="email"
+        name="email"
+        placeholder="Enter Email"
+        value={formData.email}
+        onChange={handleChange}
+      /><br /><br />
+
+      <input
+        type="number"
+        name="age"
+        placeholder="Enter Age"
+        value={formData.age}
+        onChange={handleChange}
+      /><br /><br />
+
+      <button type="submit">Save User</button>
+    </form>
+  );
+};
+
+export default UserForm;
+
